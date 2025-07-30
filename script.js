9a2d2666056c6c89d8d406f38d01dd463dfeafb7
prnv <135574321+prnv001@users.noreply.github.com>
2025-07-30 09:05:00 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+<script>
+  const hamburger = document.getElementById('hamburger');
+  const navMenu = document.getElementById('nav-menu');
+
+  hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+  });
+</script>
