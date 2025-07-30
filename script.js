79e7622991ea2cf3b2a4b7111f27f23ba7294428
prnv <135574321+prnv001@users.noreply.github.com>
2025-07-30 09:21:19 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-<script>
-  const hamburger = document.getElementById('hamburger');
-  const navMenu = document.getElementById('nav-menu');
-
-  hamburger.addEventListener('click', () => {
-    navMenu.classList.toggle('active');
-  });
-</script>
